dc01f0fb81ea6f7b07403bca5c3d2aaab9451d4c
lukasvilim <l.vilim18@gmail.com>
2022-01-29 20:51:38 +0100

    translation of whole website preps

--- src/pages/About/AboutUs.js
+++ src/pages/About/AboutUs.js
@@ -21,59 +21,59 @@ const AboutUs = () => {
                 <Typography variant='body1'>{t('about_mofito')}</Typography>
             </Paper>
             <Paper elevation={1} sx={{ my: 1, p:5}}>
-                <Typography sx={{ typography: { lg: 'h2', md: 'h3', sm: 'h4', xs: 'h5' }}}>{t('WHO ARE SSP GOLF NUTRITION?')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Sports Specific Products (SSP Golf), The Home of Golf Nutrition is a wholly owned subsidiary of the Rowallan Group. We have over 40 years experience in the sport and leisure and healthcare industries.')}</Typography>
+                <Typography sx={{ typography: { lg: 'h2', md: 'h3', sm: 'h4', xs: 'h5' }}}>{t('about_who_ssp')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_sport_scientific')}</Typography>
                 
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('FAMILY BUSINESS')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Spanning four generations, our family business has been at the forefront of innovation within the leisure industry.  Including, bowling alleys, bars, restaurants, events, amusements and the manufacture of national lottery ticket machines')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_family_business')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_spanning')}</Typography>
                 
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('INTERNATIONAL')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('SSP Golf is based in Brentwood, Essex. We also have a division in Jacksonville, Florida.  The company currently employs 55 staff that works throughout 6 businesses across the United Kingdom and the USA')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_international')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_based_in')}</Typography>
                 
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('A DECADE OF GOLF NUTRITION')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Over the last twelve years we have worked tirelessly to gain a reputation across the world for producing golf nutrition products that deliver performance, taste, quality and trust.')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_decade')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_over_twelve')}</Typography>
                 
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('INVESTMENT')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('We have been investing heavily across many areas of our business. Both innovation and technology are at the forefront of developing new products and flavours.')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_investment')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_investing')}</Typography>
                 
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('THE FUTURE')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('“Innovation and creativity are in our DNA and is at the core of everything we do at SSP.” Kevin Stack, Managing Director, SSP Golf')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_future')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_innovation')}</Typography>
  
-                <Typography sx={{my: 1}} variant='subtitle1'>{t('THE AVERAGE GOLFER BURNS 1200 CALORIES CARRYING THEIR CLUBS OVER 18 HOLES')}</Typography>
+                <Typography sx={{my: 1}} variant='subtitle1'>{t('about_average_golfer')}</Typography>
 
-                <Link to='/nutritiontips' style={{ textDecoration: 'none', color: 'black' }}><Typography variant='h5'>{t('CHECK OUT OUR HOW TO EAT FOR GOLF PAGES')}</Typography></Link>
+                <Link to='/nutritiontips' style={{ textDecoration: 'none', color: 'black' }}><Typography variant='h5'>{t('about_check_out')}</Typography></Link>
  
                 
             </Paper>
             <Paper elevation={1} sx={{ my: 1, p:5}}>
-                <Typography sx={{ typography: { lg: 'h2', md: 'h3', sm: 'h4', xs: 'h5' }}}>{t('OUR MISSION')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Lowering your score and providing golfers with the solution to back nine fatigue on the course will always be at the heart of SSP Golf.')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Our mission is to remain the world leader in golf performance nutrition with a constant commitment to developing products for golfers that will enhance their performance and lower their score.')}</Typography>
+                <Typography sx={{ typography: { lg: 'h2', md: 'h3', sm: 'h4', xs: 'h5' }}}>{t('about_mission')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_lowering')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_our_mission_is')}</Typography>
                 <img src={ourmission} alt='our mission' style={{ maxWidth: '100%', marginTop: 50}}/>
-                <Typography sx={{my: 1, mt: 5}} variant='body1'>{t('The average 18 hole round of golf burns up to an astounding 1,200 calories. That’s over 66 calories per hole. Nutrition is often neglected on the golf course, however our products are made to tackle this very problem.')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('You make sure your clubs, balls and attire are all in prime shape before setting off for a round. So why do so many people abandon their nutrition?')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Part of our mission is to use the very finest ingredients in all our products. We use the very best oats, chocolates and fruits sourced from all over the world. Each handmade bar is golden baked in our bakery in the UK. We pride ourselves on our great taste as well as functionality so you can enjoy your game to the max.')}</Typography>
-                <Typography sx={{my: 1}} variant='h5'>{t('Tackle fatigue and maximise your potential on the course.')}</Typography>
+                <Typography sx={{my: 1, mt: 5}} variant='body1'>{t('about_average_18_hole')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_make_sure_your_clubs')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_part_of_mission')}</Typography>
+                <Typography sx={{my: 1}} variant='h5'>{t('about_tackle')}</Typography>
             </Paper>
             <Paper elevation={1} sx={{ my: 1, p:5}}>
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('THE LONG AWAITED ANSWER TO GOLFER\'S BACK NINE FATIGUE')}</Typography>
-                <Typography sx={{ typography: { lg: 'h1', md: 'h2', sm: 'h3', xs: 'h4' }}}>{t('HOW DO OUR PRODUCTS WORK?')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Echoing an increasing demand from PGA Tour professionals for performance nutrition, SSP Golf Performance Bars bring tour quality performance nutrition direct to your game.')}</Typography>
-                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('MULTI-STAGE FORMULA')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Our multistage formula of simple and complex carbohydrates fuels your body and mind with both immediate and long term energy. Drip feeding you for around two hours. Ideal for combating back nine fatigue and your overall performance levels on the course.')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_the_long')}</Typography>
+                <Typography sx={{ typography: { lg: 'h1', md: 'h2', sm: 'h3', xs: 'h4' }}}>{t('about_how_do_work')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_echoing')}</Typography>
+                <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_multi_stage')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_our_multistage')}</Typography>
                 <Paper sx={{ display: 'flex', flexDirection: 'column' }}>
                     <Paper elevation={1} sx={{ my: 1, p:5}}>
-                        <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('Fuel Your Body With Performance Nutrition')}</Typography>
-                        <Typography sx={{my: 1}} variant='body1'>{t('Fuelling your body also fuels your mind and it’s your mind that makes critical decisions such as club selection, estimating distances and reading the green. A tired mind makes bad decisions.')}</Typography>
+                        <Typography sx={{ typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_fuel')}</Typography>
+                        <Typography sx={{my: 1}} variant='body1'>{t('about_fuelling')}</Typography>
                     </Paper >
                     <img src={productwork} alt='product works' style={{ maxWidth: '100%', marginTop: 50}}/>
                     </Paper>
-                <Typography sx={{ my: 1, mt: 5, typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('DON\'T CRASH AND BURN')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Too many rounds are compromised by the last few holes which can let many golfers down as fatigue and tiredness begins to kick in. With our carefully developed performance bars, you can be sure to solve this problem at its core, allowing you to finish a good round as alert and energised as you started it.')}</Typography>
-                <Typography sx={{my: 1}} variant='body1'>{t('Why not check out our Golf Nutrition pages for advice on how to eat for golf.')}</Typography>
+                <Typography sx={{ my: 1, mt: 5, typography: { lg: 'h3', md: 'h4', sm: 'h5', xs: 'h6' }}}>{t('about_dont_crash')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_too_many')}</Typography>
+                <Typography sx={{my: 1}} variant='body1'>{t('about_why_not_check')}</Typography>
             
                 <Link to='/contact' style={{ textDecoration: 'none', color: 'white' }}>
-                    <Button sx={{ m:'auto', my: 1, backgroundColor: 'rgba(0, 0, 0, 0.54)'  }} variant='contained'>{t('CLICK HERE TO ORDER')}</Button>
+                    <Button sx={{ m:'auto', my: 1, backgroundColor: 'rgba(0, 0, 0, 0.54)'  }} variant='contained'>{t('general_click_here_to_order')}</Button>
                 </Link>
             </Paper>
         </Box>
--- src/pages/Trade/Trade.js
+++ src/pages/Trade/Trade.js
@@ -76,12 +76,10 @@ const Trade = () => {
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                     >
-                        <Typography>What is the Golf Performance Energy Bar?</Typography>
+                        <Typography>{t('trade_what_is_GPEB')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                            The Golf Performance Energy bar is a multi-stage performance energy bar that has been developed for the energy needs of golfers only.
-                        </Typography>
+                        <Typography>{t('trade_GPEB_is')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -91,17 +89,10 @@ const Trade = () => {
                     aria-controls="panel2a-content"
                     id="panel2a-header"
                     >
-                        <Typography>Why does a golfer need a specific energy bar?</Typography>
+                        <Typography>{t('trade_why_does')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                            This question can take many different angles, all of which are of relevance:
-                            1.The energy requirement for golf is different than other sports/activities.
-                            2.The Golf Performance Energy Bars have the perfect amount of calories to give the golfer the performance energy he/she needs without having them feel hungry or full.
-                            3.The bars have both simple carbohydrates and complex carbohydrates (your instant and long term energy sources), which will improve your game by giving you the right energy at the right time.
-                            4.These bars are made from real food ingredients – oats, rice, wheat along with real fruit and Belgian Dark Chocolate – not powders like most energy bars.  This gives the bars a great taste which we always receive compliments on!
-                            5.100% natural (no artificial colours, flavourings and preservatives)
-                    </Typography>
+                        <Typography>{t('trade_this_question')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -111,12 +102,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>How does the bar actually work?</Typography>
+                        <Typography>{t('trade_how_does_work')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                            As you consume the bar the simple carbohydrates (instant energy) release into the bloodstream for the working muscles and brain, while the complex carbohydrates (long term energy) get stored and then start to slowly release after around 15-20 minutes. This gives the golfer a steady supply of the right type of energy which lasts for over nine holes. In turn, this aids in enhancing your performance while simultaneously providing you with a tasty, satisfying snack.
-                    </Typography>
+                        <Typography>{t('trade_as_you_consume')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -126,12 +115,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>How do you use the bar?</Typography>
+                        <Typography>{t('trade_how_do_you')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                        It is recommended that you eat 1/3 of a bar every three holes beginning on the 10th; particularly on the Back 9 when your body has an energy level deficit. This allows the simlpe and complex carbohydrates to be fed into the body in the most efficient manner
-                    </Typography>
+                        <Typography>{t('trade_recommended')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -141,14 +128,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>What about bananas, healthy food bars and sports bars?</Typography>
+                        <Typography>{t('trade_what_about')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                        The calories in a banana last for 2-3 holes, and the type of energy you get from a banana depends on how ripe it is.  The riper the banana the more sugar it has and the faster it releases into the bloodstream. The greener the banana the more starch it has and the slower it releases into your bloodstream.  You would need to eat between 3-4 bananas for the energy required to play 9 holes of golf.
-                        A healthy food bar is just what it says it is – something that’s healthy to eat. It is not made with any thoughts to what you need to get the most out of your golf game.  Normally, these bars are low calories and do not have sufficient carbohydrates (energy) for the game of golf.
-                        Sport drinks are basically sugar water with a minimal amount of electrolytes (sodium, potassium etc).  Large amounts of electrolytes are lost during intense cardiovascular activity – long distance running, biking, sprinting, etc.
-                    </Typography>
+                        <Typography>{t('trade_calories')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -158,13 +141,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>Why do the bars have over 400 calories?</Typography>
+                        <Typography>{t('trade_why_do_the_bars')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                        Golfers are normally on the course for 3.5- 4 hours and during that time they will walk on average 4-5 miles, along with pushing a trolley or carrying their bags, practice swings, etc. This means that a male of average height and weight will be using between 1000-1300 calories (larger males will use more calories). Women use between 800-1000+ calories over 18 holes.
-                        There are a certain amount of calories a golfer needs to re-fuel his/her system without making them feel too full or hungry. Because my background is in sports kinesiology, I calculated and developed a bar that would slowly release energy based on the above. With this bar, and when eaten a 1/3 every 3 holes, the golfer is able to satisfy those energy requirements without feeling hungry or full.
-                    </Typography>
+                        <Typography>{t('trade_golfers')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -174,12 +154,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>Isn't 400 calories too many for the female golfer?</Typography>
+                        <Typography>{t('trade_isnt_400_much')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                        NO! The reason being that even though women may weigh less than men they work just as hard over 18 holes and need to replenish their energy systems if they want to reach maximum performance.
-                    </Typography>
+                        <Typography>{t('trade_no')}</Typography>
                     </AccordionDetails>
                 </Accordion>
 
@@ -189,13 +167,10 @@ const Trade = () => {
                     aria-controls="panel3a-content"
                     id="panel3a-header"
                     >
-                        <Typography>Information about ingredients and nutritional values?</Typography>
+                        <Typography>{t('trade_info')}</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                        The first ingredient in our bars is oats, along with rice crisps, wheat and real fruit (Apricots, Blueberries, Cranberries, Goji berries and Raisins). For our Chocolate/Toffee Chip bar we use Belgian Dark Chocolate – known for its incredible flavour. We then bake the bars which give them a taste and texture second to none.
-                        Since we are all concerned with the saturated fat content in the foods we eat, the Golf Performance Energy Bars have only 9-10% depending on which bar you eat, which runs along the same guidelines that the government recommends for health. If you’re using the golf performance bar while on the course, your body is burning the small amount of saturated fat in the form of energy from the bar! Every calorie in these bars is utilised as energy- not one calorie will be stored in your system!
-                    </Typography>
+                        <Typography>{t('trade_first_ingredient')}</Typography>
                     </AccordionDetails>
                 </Accordion>
             </Paper>
